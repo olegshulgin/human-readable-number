88ad8b99cfdc767f1b1070034a5b17dc694fac3f
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-06-26 21:47:09 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+  const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+  const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    
+  if (number < 20) {
+    return num[number];
+  }  
+  let digit = number%10;
+  if (number < 100) {
+    return tens[~~(number/10)-2] + (digit? " " + num[digit]: "");
+  }  
+  if (number < 1000) {
+    return num[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+  }  
 }
